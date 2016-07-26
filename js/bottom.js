c3e5e647faa5adc57308c512653853af873f4a5a
KnutMonason <knutmonason@gmail.com>
2016-07-26 03:33:22 +0200

    Last for tonight

--- js/bottom.js
+++ js/bottom.js
@@ -1,7 +1,7 @@
 
 $(window).scroll(function() {
-   $(".return-bottom").removeClass("visible");
-  if ($(window).scrollTop() >= $(document).height() - $(window).height() - 300) {
+
+  if ($(window).scrollTop() >= $(document).height() - $(window).height() - 250) {
        //you are at bottom
        $(".return-bottom").addClass("visible");
 
